08c55d018820dc73a68a51867d12ef8b9c210bdf
gardoroman <gardoroman@gmail.com>
2021-06-10 21:49:54 -0500

    WIP: Begin work on selecting subsection of image to apply pixelation.

--- scripts/pixelator.js
+++ scripts/pixelator.js
@@ -2,6 +2,7 @@
 const pixelator = (()=>{
 
     let canvas, ctx, originalImage;
+    let partialSelection = {};
     const init = ()=> {
       canvas = document.querySelector('canvas');
       ctx = canvas.getContext("2d");
@@ -29,33 +30,47 @@ const pixelator = (()=>{
      * Loop through image array and overwrite the 
      * rgba values for the surrounding pixels by
      * the rgba value of the first pixel in the iteration.
+     * 
+     * If width and height contain integer values, the function
+     * was called by the user selection of a portion of
+     * the image.
      */
     const drawPixels = (width, height) => {
         let boxSize = 5;
 
-        let imageWidth = originalImage.width;
-        let imageHeight = originalImage.height;
+        let selectionWidth = originalImage.width;
+        let selectionHeight = originalImage.height;
 
-        let imageData = ctx.getImageData(0, 0, imageWidth, imageHeight);
+        let imageData = ctx.getImageData(0, 0, selectionWidth, selectionHeight);
 
         let pixelData = imageData.data;
 
-        for(let y=0; y < imageHeight; y += boxSize ){
-            for(let x=0; x < imageWidth; x+= boxSize){
+        for(let y=0; y < selectionHeight; y += boxSize ){
+            for(let x=0; x < selectionWidth; x += boxSize){
                 // each pixel is represented by four rgba values in the imageData.data Uint8ClampedArray array
                 // multiply by four to get the index for the next set of rgba values.
-                let index = (x + (y * imageWidth)) * 4;
+                let index = (x + (y * selectionWidth)) * 4;
                 
                 ctx.fillStyle = `rgba(${pixelData[index]}, ${pixelData[index+1]}, ${pixelData[index+2]}, ${pixelData[index+3]})`;
-                let rectWidth = adjustDimension(x, boxSize, imageWidth);
-                let rectHeight = adjustDimension(y, boxSize, imageHeight);
+                let rectWidth = adjustDimension(x, boxSize, selectionWidth);
+                let rectHeight = adjustDimension(y, boxSize, selectionHeight);
                 ctx.fillRect(x,y, rectWidth, rectHeight);
             }
         }
     };
+
+    const handleMouseUp = event => {
+    
+        let e = event;
+        let o = originalImage
+        console.log('hover hover')
+    }
+
+    const handleMouseDown = event => {
+        console.log(`x: ${event.x} y: ${event.y}`)
+    }
   
     const getImage = image => {
-  
       /* Note:
       The code below keeps the ratio of the image.
       Keep for future implementations to give user option to retain original ratio.
@@ -86,6 +101,10 @@ const pixelator = (()=>{
       let btn = document.getElementById('btn-pixelate')
       btn.style.display = 'block';
       btn.addEventListener('click', drawPixels, false)
+
+      let canvasElement = document.getElementById('canvas-elem');
+      canvasElement.addEventListener('mousedown', handleMouseDown, false);
+      canvasElement.addEventListener('mouseup', handleMouseUp, false);
   
     };
   
